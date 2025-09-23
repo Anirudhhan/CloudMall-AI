fd209f48ee28c0ef06088d5ae7b22574b21ff87b
Anirudhhan <anirudhhan.ashok@gmail.com>
2025-09-23 20:03:03 +0530

    backend

---
+++ backend/src/test/java/com/cloudmall/ai/cloudMallAI/CloudMallAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudmall.ai.cloudMallAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudMallAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
